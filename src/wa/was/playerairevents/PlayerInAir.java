fddba374b552a4d48aca6b0aea1dccb0b51dcb09
Jordan Thompson <jordanslost@gmail.com>
2017-04-29 12:59:22 -0700

    Version 0.0.4
    Removed PlayerAirborneEvent

--- src/wa/was/playerairevents/PlayerInAir.java
+++ src/wa/was/playerairevents/PlayerInAir.java
@@ -15,31 +15,30 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import wa.was.playerairevents.events.PlayerAirborneEvent;
 import wa.was.playerairevents.events.PlayerFallEvent;
 import wa.was.playerairevents.events.PlayerJumpEvent;
 import wa.was.playerairevents.events.PlayerLandedEvent;
 import wa.was.playerairevents.events.PlayerRisingEvent;
 import wa.was.playerairevents.spigot.events.PlayerExitsEvent;
 
-@SuppressWarnings("deprecation")
-public class PlayerInAir extends JavaPlugin implements Listener {
-	
+public final class PlayerInAir extends JavaPlugin implements Listener {
+
 	private static PlayerInAir instance;
-	
+
 	public static PlayerInAir getInstance() {
 		return instance;
 	}
+
 	private Map<UUID, Integer> hasFallen;
 	private Map<UUID, Integer> hasJumped;
-	
+
 	private Map<UUID, Boolean> wasAirborn;
 	private Map<UUID, Location> fallLocation;
-	
+
 	public PlayerInAir() {
 		instance = this;
 	}
-	
+
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
@@ -49,89 +48,90 @@ public class PlayerInAir extends JavaPlugin implements Listener {
 		hasFallen = new HashMap<UUID, Integer>();
 		fallLocation = new HashMap<UUID, Location>();
 	}
-	
-	@EventHandler(priority=EventPriority.LOWEST)
+
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void playerInAir(PlayerMoveEvent e) {
 		Player player = e.getPlayer();
-		if ( player.getEyeLocation().getBlock().getType().equals(Material.WATER)
+		if (player.getEyeLocation().getBlock().getType().equals(Material.WATER)
 				|| player.getEyeLocation().getBlock().getType().equals(Material.STATIONARY_WATER)
-				|| player.getEyeLocation().getBlock().getType().equals(Material.LADDER) )  {
-			hasJumped.remove(player.getUniqueId());
-			hasFallen.remove(player.getUniqueId());
-			wasAirborn.put(player.getUniqueId(), false);
+				|| player.getEyeLocation().getBlock().getType().equals(Material.LADDER)) {
+			resetPlayer(player);
 			return;
 		}
-		if ( player.isOnGround() )  {
+		if (player.isOnGround()) {
 			resetPlayer(player);
 			return;
 		}
 		Location f = e.getFrom(), t = e.getTo();
 		// Jump
-		if ( f.getBlock().getY() < t.getBlock().getY()
-				&& ! ( f.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR) )
-				&& t.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR) ) {
-			if ( ! ( wasAirborn.containsKey(player.getUniqueId()) ) ) {
+		if (f.getBlock().getY() < t.getBlock().getY()
+				&& !(f.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR))
+				&& t.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR)) {
+			if (!(wasAirborn.containsKey(player.getUniqueId()))) {
 				wasAirborn.put(player.getUniqueId(), true);
 			}
 			int jumped = 1;
-			if ( hasJumped.containsKey(player.getUniqueId()) ) {
+			if (hasJumped.containsKey(player.getUniqueId())) {
 				jumped = hasJumped.get(player.getUniqueId());
 			}
-			Bukkit.getServer().getPluginManager().callEvent(new PlayerJumpEvent(player, jumped, e.getFrom(), e.getTo()));
-		// Airborne
-		} else if ( f.getBlock().getY() < t.getBlock().getY()
-				&& t.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR) ) {
-			if ( ! ( wasAirborn.containsKey(player.getUniqueId()) ) ) {
+			Bukkit.getServer().getPluginManager()
+					.callEvent(new PlayerJumpEvent(player, jumped, e.getFrom(), e.getTo()));
+			// Airborne
+		} else if (f.getBlock().getY() < t.getBlock().getY()
+				&& t.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR)) {
+			if (!(wasAirborn.containsKey(player.getUniqueId()))) {
 				wasAirborn.put(player.getUniqueId(), true);
 			}
-			if ( hasJumped.containsKey(player.getUniqueId()) ) {
+			if (hasJumped.containsKey(player.getUniqueId())) {
 				int jumped = hasJumped.get(player.getUniqueId());
-				hasJumped.put(player.getUniqueId(), jumped+1);
+				hasJumped.put(player.getUniqueId(), jumped + 1);
 			} else {
 				hasJumped.put(player.getUniqueId(), 1);
 			}
 			int jumped = hasJumped.get(player.getUniqueId());
-			Bukkit.getServer().getPluginManager().callEvent(new PlayerAirborneEvent(player, jumped, e.getFrom(), e.getTo()));
-			Bukkit.getServer().getPluginManager().callEvent(new PlayerRisingEvent(player, jumped, e.getFrom(), e.getTo()));
-		// Falling
-		} else if ( f.getBlock().getY() > t.getBlock().getY() 
-				&& t.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR) ) {
-			if ( ! ( wasAirborn.containsKey(player.getUniqueId()) ) ) {
+			Bukkit.getServer().getPluginManager()
+					.callEvent(new PlayerRisingEvent(player, jumped, e.getFrom(), e.getTo()));
+			// Falling
+		} else if (f.getBlock().getY() > t.getBlock().getY()
+				&& t.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR)) {
+			if (!(wasAirborn.containsKey(player.getUniqueId()))) {
 				wasAirborn.put(player.getUniqueId(), true);
 			}
-			if ( ! ( fallLocation.containsKey(player.getUniqueId()) ) ) {
+			if (!(fallLocation.containsKey(player.getUniqueId()))) {
 				fallLocation.put(player.getUniqueId(), f.getBlock().getLocation());
 			}
-			int fallen = (int) Math.abs(( fallLocation.get(player.getUniqueId()).getY() - t.getBlock().getLocation().getY() ));
+			int fallen = (int) Math
+					.abs((fallLocation.get(player.getUniqueId()).getY() - t.getBlock().getLocation().getY()));
 			hasFallen.put(player.getUniqueId(), fallen);
 			int jumped = 1;
-			if ( hasJumped.containsKey(player.getUniqueId()) ) {
+			if (hasJumped.containsKey(player.getUniqueId())) {
 				jumped = hasJumped.get(player.getUniqueId());
 			}
-			Bukkit.getServer().getPluginManager().callEvent(new PlayerFallEvent(player, fallen, jumped, e.getFrom(), e.getTo(), fallLocation.get(player.getUniqueId())));
-		// Landed (?)
-		} else if ( wasAirborn.containsKey(player.getUniqueId()) 
-				&& wasAirborn.get(player.getUniqueId()) 
-				|| ( t.getBlock().getRelative(BlockFace.DOWN, 1).getType().isSolid()
-				&& f.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR) ) ) {
+			Bukkit.getServer().getPluginManager().callEvent(new PlayerFallEvent(player, fallen, jumped, e.getFrom(),
+					e.getTo(), fallLocation.get(player.getUniqueId())));
+			// Landed (?)
+		} else if (wasAirborn.containsKey(player.getUniqueId()) && wasAirborn.get(player.getUniqueId())
+				|| (t.getBlock().getRelative(BlockFace.DOWN, 1).getType().isSolid()
+						&& f.getBlock().getRelative(BlockFace.DOWN, 1).getType().equals(Material.AIR))) {
 			int jumped = 1;
 			int fallen = 1;
-			if ( hasJumped.containsKey(player.getUniqueId()) ) {
+			if (hasJumped.containsKey(player.getUniqueId())) {
 				jumped = hasJumped.get(player.getUniqueId());
 			}
-			if ( hasFallen.containsKey(player.getUniqueId()) ) {
+			if (hasFallen.containsKey(player.getUniqueId())) {
 				fallen = hasFallen.get(player.getUniqueId());
 			}
-			Bukkit.getServer().getPluginManager().callEvent(new PlayerLandedEvent(player, fallen, jumped, e.getFrom(), e.getTo()));
+			Bukkit.getServer().getPluginManager()
+					.callEvent(new PlayerLandedEvent(player, fallen, jumped, e.getFrom(), e.getTo()));
 			resetPlayer(player);
 		}
 	}
-	
+
 	public void resetPlayer(Player player) {
 		hasJumped.remove(player.getUniqueId());
 		hasFallen.remove(player.getUniqueId());
 		wasAirborn.put(player.getUniqueId(), false);
 		fallLocation.remove(player.getUniqueId());
 	}
-	
+
 }
